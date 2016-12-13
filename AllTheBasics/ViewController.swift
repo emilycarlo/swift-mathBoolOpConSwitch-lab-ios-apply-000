7d717682f4b3918480106c0c748a371f8fcbb38d
Emily Carlo <EmilyCarlo@172-16-183-173.DYNAPOOL.NYU.EDU>
2016-12-13 13:29:03 -0500

    Complete problems

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -5,31 +5,31 @@
 //  Created by James Campagno on 6/28/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
-
 func averageIsAbove(_ a: Double, _ b: Double, c: Double) -> Bool {
-return ((a + b + c) / 3.0) > 75.0
+    return ((a + b + c) / 3.0) > 75.0
 }
 
 func passwordCombo(username: String, password: Int) -> String {
-switch username {
-case "Jerry", "Elaine", "Michael":
-if password % 3 == 0 { return "Welcome!" }
-return "Access Denied"
-default:
-return "Access Denied"
-     }
+    switch username {
+    case "Jerry", "Elaine", "Michael":
+        if password % 3 == 0 { return "Welcome!" }
+        return "Access Denied"
+    default:
+        return "Access Denied"
+    }
 }
 func describe(emoji: String) -> String {
-switch emoji {
-case "💋":
-return "Kiss"
-case "🐈":
-return "Cat"
-case "🐢":
-return "Turtle"
-case "👻":
-return "Ghost"
-default:
-return "Unknown"
+    switch emoji {
+    case "💋":
+        return "Kiss"
+    case "🐈":
+        return "Cat"
+    case "🐢":
+        return "Turtle"
+    case "👻":
+        return "Ghost"
+    default:
+        return "Unknown"
+    }
 }
-}
+
