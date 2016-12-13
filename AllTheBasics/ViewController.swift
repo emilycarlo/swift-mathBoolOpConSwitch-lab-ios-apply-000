7a60818cfe9c28a1936afa6ee4dd644c16c89eb7
Emily Carlo <EmilyCarlo@172-16-183-173.DYNAPOOL.NYU.EDU>
2016-12-13 13:01:38 -0500

    Complete problems

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -6,16 +6,32 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
-import UIKit
+func averageIsAbove(_ a: Double, _ b: Double, c: Double) -> Bool {
+return ((a + b + c) / 3.0) > 75.0
+}
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    // Implement your functions here!
-    
-
+func passwordCombo(username: String, password: Int) -> String {
+switch username {
+case "Jerry", "Elaine", "Michael":
+if password % 3 == 0 { return "Welcome!" }
+return "Access Denied"
+default:
+return "Access Denied"
+     }
+}
+func describe(emoji: String) -> String {
+switch emoji {
+case "💋":
+return "Kiss"
+case "🐈":
+return "Cat"
+case "🐢":
+return "Turtle"
+case "🍕":
+return "Pizza"
+case "👻":
+return "Ghost"
+default:
+return "Unknown"
+}
 }
